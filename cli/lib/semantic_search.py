9d81c67ab1316771a027afe2ac101a732ec3ff0a
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-22 20:34:38 +0200

    create embeddings using model

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -5,6 +5,19 @@ class SemanticSearch:
     def __init__(self):
         self.model: SentenceTransformer = SentenceTransformer("all-MiniLM-L6-v2")
 
+    def generate_embedding(self, text: str):
+        text = text.strip()
+        if len(text) == 0:
+            raise ValueError("Text cannot be empty")
+        return self.model.encode([text])[0]
+
+
+def embed_text(text: str):
+    embedding = SemanticSearch().generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
 
 def verify_model():
     ss = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.semantic_search import verify_model
+from lib.semantic_search import embed_text, verify_model
 
 
 def main():
@@ -11,11 +11,16 @@ def main():
 
     _ = subparsers.add_parser("verify", help="Verify the model")
 
+    embed_text_parser = subparsers.add_parser("embed_text", help="Embed text")
+    _ = embed_text_parser.add_argument("text", help="Text to embed")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
