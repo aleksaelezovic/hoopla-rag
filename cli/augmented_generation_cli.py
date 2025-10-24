f00413f4ca9c12bb476a5f7b0a1dfb008287e7fd
aleksaelezovic <aleksaelezovic@gmail.com>
2025-10-24 15:47:08 +0200

    get LLM summary for a query

--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -20,6 +20,14 @@ def main():
     )
     _ = rag_parser.add_argument("query", type=str, help="Search query for RAG")
 
+    summarize_parser = subparsers.add_parser(
+        "summarize", help="Generate a summary of a search"
+    )
+    _ = summarize_parser.add_argument("query", type=str, help="Search Query")
+    _ = summarize_parser.add_argument(
+        "--limit", help="Search results limit", type=int, default=5
+    )
+
     args = parser.parse_args()
 
     match args.command:
@@ -47,6 +55,33 @@ def main():
 
             print("RAG Response:")
             print(rag_response)
+        case "summarize":
+            api_key = os.environ.get("GEMINI_API_KEY")
+            client = genai.Client(api_key=api_key)
+            query = args.query
+            limit = args.limit
+            res = HybridSearch(load_movies()).rrf_search(query, 60, limit)
+            prompt = f"""
+            Provide information useful to this query by synthesizing information from multiple search results in detail.
+            The goal is to provide comprehensive information so that users know what their options are.
+            Your response should be information-dense and concise, with several key pieces of information about the genre, plot, etc. of each movie.
+            This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+            Query: {query}
+            Search Results:
+            {json.dumps(res)}
+            Provide a comprehensive 3–4 sentence answer that combines information from multiple sources:
+            """
+            summary = client.models.generate_content(
+                model="gemini-2.0-flash-001", contents=prompt
+            ).text
+
+            print("Search Results:")
+            for doc in res:
+                print(f"- {doc.get('title', '<error unknown title>')}")
+            print()
+
+            print("LLM Summary:")
+            print(summary)
         case _:
             parser.print_help()
 
